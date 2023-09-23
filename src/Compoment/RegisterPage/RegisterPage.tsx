20f126818f1e130c31e8e99f1307e6edf92b149b
vy <nguyentuongvy20121997@gmail.com>
2023-09-23 11:56:03 +0700

    i

--- src/Compoment/RegisterPage/RegisterPage.tsx
+++ src/Compoment/RegisterPage/RegisterPage.tsx
@@ -17,6 +17,8 @@ import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import FormControl from "@mui/material/FormControl";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import { IconButton, OutlinedInput } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 interface Values {
   firstName: string;
   lastName: string;
@@ -56,19 +58,36 @@ const RegisterPage = () => {
   });
 
   const { handleSubmit, values, handleChange, errors, resetForm } = formik;
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
   return (
     <div className="signup-wrapper">
       <form className="signup-form-container" onSubmit={handleSubmit}>
         <h2>Create an account</h2>
         <div className="form-control">
           <TextField
-            label="fullname"
-            variant="outlined"
             id="fullname"
             name="fullname"
+            label="Fullname"
             value={values.fullname}
             onChange={handleChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
           />
+
           {errors.fullname && (
             <p className="error-message">{errors.fullname}</p>
           )}
@@ -95,6 +114,30 @@ const RegisterPage = () => {
             <p className="error-message">{errors.password}</p>
           )}
         </div>
+
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+
         <button className="btn-submit">Submit</button>
       </form>
     </div>
